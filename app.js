c3ebbb7f93e91e15a568f038e2a8146a77acda8e
Ikhlak <ikhlak789@gmail.com>
2024-11-25 10:44:35 -0800

    resolved exercise

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-$(function() {
-  // YOUR CODE HERE
-})
+$(function () {
+  $("#toggleButton").on("click", function () {
+    $("#myParagraph").toggle();
+  });
+
+  $("#colorButton").on("click", function () {
+    $("#colorDiv").css({
+      "background-color": "yellow",
+    });
+  });
+
+  $("#addClassButton").on("click", function () {
+    $("#classDiv").addClass("new-class");
+  });
+
+  $("#removeClassButton").on("click", function () {
+    $("#classDiv").removeClass("new-class");
+  });
+
+  $("#fadeInButton").on("click", function () {
+    $("#fadeDiv").fadeIn("new-class");
+  });
+
+  $("#fadeOutButton").on("click", function () {
+    $("#fadeDiv").fadeOut("new-class");
+  });
+
+  $("#slideUpButton").on("click", function () {
+    $("#slideDiv").slideUp("new-class");
+  });
+
+  $("#slideDownButton").on("click", function () {
+    $("#slideDiv").slideDown("new-class");
+  });
+});
